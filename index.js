6296e8baf02d0c52718f9ca220dbe154424692d5
Ijaj Ahamed Nabil <37688324+ian-of-yore@users.noreply.github.com>
2022-11-05 21:04:55 +0600

    connected with mongoDB atlas

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 const app = express();
 
@@ -12,6 +13,23 @@ app.use(express.json());
 app.get('/', (req, res) =>{
     res.send('ema john server running');
 })
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.mmmt3qa.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+
+    }
+    finally{
+
+    }
+}
+
+run().catch( (error) => console.error(error));
+
+
 
 app.listen(port, () => {
     console.log(`Server running at port ${port}`)
