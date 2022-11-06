c08a176fea22df59436696442d3315468e8f9694
Ijaj Ahamed Nabil <37688324+ian-of-yore@users.noreply.github.com>
2022-11-06 19:44:19 +0600

    Pagination done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 const app = express();
 
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 
-app.get('/', (req, res) =>{
+app.get('/', (req, res) => {
     res.send('ema john server running');
 })
 
@@ -18,29 +18,37 @@ app.get('/', (req, res) =>{
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.mmmt3qa.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-async function run(){
-    try{
+async function run() {
+    try {
         const database = client.db("emaJohn");
         const productsCollection = database.collection("products");
 
-        app.get('/products', async(req, res) =>{
-            const currentPage = req.query.currentPage;
-            const perPageDocument = req.query.perPageDocument;
-            console.log(currentPage, perPageDocument);
-            
+        app.get('/products', async (req, res) => {
+            const currentPage = parseInt(req.query.currentPage);
+            const perPageDocument = parseInt(req.query.perPageDocument);
+
             const query = {};
             const cursor = productsCollection.find(query);
-            const products = await cursor.toArray();
+            const products = await cursor.skip(currentPage * perPageDocument).limit(perPageDocument).toArray();
             const count = await productsCollection.estimatedDocumentCount();
-            res.send({count, products});
+            res.send({ count, products });
+        })
+
+        app.post('/productsByIds', async (req, res) => {
+            const ids = req.body;
+            const objectIds = ids.map(id => ObjectId(id));
+            const query = { _id: { $in: objectIds } };
+            const cursor = productsCollection.find(query);
+            const productsByIds = await cursor.toArray();
+            res.send(productsByIds);
         })
     }
-    finally{
+    finally {
 
     }
 }
 
-run().catch( (error) => console.error(error));
+run().catch((error) => console.error(error));
 
 
 
